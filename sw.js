ec4a22e812ba9b441310a7668e3069d409a7dd39
Saneas26 <saneacuerpoymente@gmail.com>
2026-07-24 00:52:28 +0100

    v2.2: splash animado de arranque + tarjeta Saneas rediseñada (estilo web, eslogan bicolor)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pdv-v4";
+const CACHE = "pdv-v5";
 const ASSETS = ["./", "index.html", "rutas.js", "manifest.json", "icon.svg", "icon-192.png", "icon-512.png", "quicksand-bold.woff2"];
 
 self.addEventListener("install", e => {
