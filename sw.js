18c6137ebb3369354b366e6c3b853a5c3b1eef30
Saneas26 <290082599+Saneas26@users.noreply.github.com>
2026-08-11 23:16:15 +0100

    Grupo Saneas: nueva app ¿Qué me dices? (parrilla del pie + desplegable + QR de compartir)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = "pdv-v20";
+const CACHE = "pdv-v21";
 const ASSETS = ["./", "index.html", "rutas.js", "manifest.json", "icon-192.png", "icon-512.png", "quicksand-bold.woff2", "montserrat-bold.woff2",
-  "js/24-grupo-saneas.js", "img/app-saneas-web.png", "img/app-saneas-s.png", "img/app-asesorias.png", "img/app-pordondevoy.png", "img/app-activala.png", "img/app-laora.png", "img/app-acumula.png",
+  "js/24-grupo-saneas.js", "img/app-saneas-web.png", "img/app-saneas-s.png", "img/app-asesorias.png", "img/app-pordondevoy.png", "img/app-activala.png", "img/app-laora.png", "img/app-acumula.png", "img/app-quemedices.png",
   "planes/", "planes/index.html",
   "planes/lanzarote/", "planes/lanzarote/index.html", "planes/lanzarote/lanzarote.css", "planes/lanzarote/lanzarote.js", "planes/lanzarote/lanzarote-og.png", "planes/lanzarote/lanzarote-favicon.svg"];
 
