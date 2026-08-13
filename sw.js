4f98817ecfa71156e3fea699b3014d7c2f4ec2a4
Saneas26 <saneacuerpoymente@gmail.com>
2026-08-13 15:38:29 +0100

    Añade miniatura de Tenerife a la tarjeta del catálogo

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE = "pdv-v26";
+const CACHE = "pdv-v27";
 const ASSETS = ["./", "index.html", "rutas.js", "manifest.json", "icon-192.png", "icon-512.png", "quicksand-bold.woff2", "montserrat-bold.woff2",
   "js/24-grupo-saneas.js", "img/app-saneas-web.png", "img/app-saneas-s.png", "img/app-asesorias.png", "img/app-pordondevoy.png", "img/app-activala.png", "img/app-laora.png", "img/app-acumula.png",
   "planes/", "planes/index.html",
   "planes/lanzarote/", "planes/lanzarote/index.html", "planes/lanzarote/lanzarote.css", "planes/lanzarote/lanzarote.js", "planes/lanzarote/lanzarote-og.png", "planes/lanzarote/lanzarote-card.png", "planes/lanzarote/lanzarote-favicon.svg",
   "planes/gran-canaria/", "planes/gran-canaria/index.html", "planes/gran-canaria/gc.css", "planes/gran-canaria/gc.js", "planes/gran-canaria/og.png", "planes/gran-canaria/favicon.svg",
-  "planes/tenerife/", "planes/tenerife/index.html", "planes/tenerife/tenerife.css", "planes/tenerife/tenerife.js", "planes/tenerife/favicon.svg"];
+  "planes/tenerife/", "planes/tenerife/index.html", "planes/tenerife/tenerife.css", "planes/tenerife/tenerife.js", "planes/tenerife/tenerife-card.png", "planes/tenerife/favicon.svg"];
 
 self.addEventListener("install", e => {
   e.waitUntil(
